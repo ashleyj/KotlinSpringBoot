56cd7f842d87f5891a274fb4268af63e2fea2066
Ashley <ashley.wil@gmail.com>
2017-05-23 19:46:30 +1000

    IJ spring plugin doesn't like top level functions

--- src/main/kotlin/com/example/demo/KotlinSpringApplication.kt
+++ src/main/kotlin/com/example/demo/KotlinSpringApplication.kt
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class KotlinSpringApplication
+open class KotlinSpringApplication {
 
-fun main (args: Array<String>) {
-    SpringApplication.run(KotlinSpringApplication::class.java, *args);
+    companion object {
+        @JvmStatic fun main (args: Array<String>) {
+            SpringApplication.run(KotlinSpringApplication::class.java, *args);
+        }
+    }
 }
+
 
 
